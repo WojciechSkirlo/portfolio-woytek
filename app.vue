cc068c07f37b63ae43c1e1f8ba861de8a2dea6c4
Wojciech Skirło <wojciechskiro@gmail.com>
2022-11-15 19:30:15 +0100

    update scrollbar

--- app.vue
+++ app.vue
@@ -1,5 +1,15 @@
 <template>
-  <div class="antialiased">
+  <div class="relative h-screen">
     <NuxtPage />
   </div>
 </template>
+<script setup lang="ts">
+// import Scrollbar from "smooth-scrollbar";
+// import MeowPlugin from "meow-plugin";
+
+onMounted(() => {
+  // Scrollbar.use(MeowPlugin);
+  // Scrollbar.init(document.querySelector("[data-scrollbar]")!);
+  // console.log("scroll", Scrollbar);
+});
+</script>
